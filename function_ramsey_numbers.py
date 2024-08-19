bac47a6ea13ec00036ccc2c76ca84622e2106100
AcengAkii <2432329@students.wits.ac.za>
2024-08-19 20:04:22 +0200

    Work_so_far

---
+++ function_ramsey_numbers.py
@@ -0,0 +1,9 @@
+# This file must be able to determine the smaller Ramsey numbers
+import function_adjacency as aj
+import function_grouping_homomorphics as gh
+
+
+def Rams(k,l):
+    
+    
+    return
